57c5ed5822ed6fecd2205b5b36cf5ad20c3782a7
dawid86 <dawid_wroblewski@o2.pl>
2021-06-15 10:15:42 +0200

    adding condition

--- Ex3/ex3.py
+++ Ex3/ex3.py
@@ -1,3 +1,11 @@
-hrs = input("hours weekly: ")
-rate = input("rate per hour: ")
+# remember that names of the variables comes first, like hrs or rate
+
+hrs = input("Enter Hours: ")
+rate = input("Rate per hour: ")
+h = float(hrs)
+r = float(rate)
+if h > 40
+    print(h*r*1.5)
+else
+    print(h*r)
 print("pay",float(hrs)*float(rate))
