c927a2d87fffc677fa3395a16c8893ab46be4eb1
dawid86 <dawid_wroblewski@o2.pl>
2021-06-15 10:37:14 +0200

    finalized excercise

--- Ex3/ex3.py
+++ Ex3/ex3.py
@@ -4,7 +4,8 @@ hrs = input("Enter Hours: ")
 rate = input("Rate per hour: ")
 h = float(hrs)
 r = float(rate)
-if h > 40.0
-    print(h*r*1.5)
-else
+
+if h < 40.0 :
     print(h*r)
+else :
+    print(40*r+((h-40)*(r*1.5)))
